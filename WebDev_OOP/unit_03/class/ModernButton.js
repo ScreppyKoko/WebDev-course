c1c4c05550d1620425bf9652db603607eba70ed1
Screppy Coco <kozel95@bk.ru>
2022-03-11 06:54:41 +0300

    completed tasks from unit_3

--- WebDev_OOP/unit_03/class/ModernButton.js
+++ WebDev_OOP/unit_03/class/ModernButton.js
@@ -2,27 +2,34 @@
 // <p>Создайте класс ModernButton который наследуется от класса Button. Добавьте через конструктор свойство borderRadius,
 // которое по умолчанию равно нулю. Свойство принимает целое число. </p>
 
-class ModernButton extends Button {
-   constructor(width, height, background, value, borderRadius) {
-      super(width, height, background, value);
-      this.borderRadius = borderRadius;
-   }
-   render() {
-      let btn = document.createElement('button');
-      super.render();
-      return btn.style.borderRadius = this.borderRadius + 'px';
-   }
-}
-
 // Task 4
 // <p>Поскольку класс ModernButton наследуется от Button, то он уже имеет метод render() и мы его можем применить.
 // Но туда не входит свойство borderRadius. Давайте допишем данное свойство. Создайте метод render, внутри ModernButton.
 // Чтобы не делать лишний код, мы можем получить через super метод render родителя - который возвращает готовый объект.
 // Добавим ему свойство borderRadius и возвратим данный объект через return.</p>
 
-let btnMod = new ModernButton(300, 200, 'yellow', 'working', 25);
-btnMod.render();
-console.log(btnMod);
-
 // Task 5.
 // <p>Создайте экземпляр класса ModernButton и выведите его в body на странице.</p>
+
+
+class ModernButton extends Button {
+   constructor(width, height, background, value, borderRadius = 0) {
+      super(width, height, background, value);
+      this.borderRadius = Math.round(borderRadius);
+   }
+   render() {
+      // super.render() 
+      let btn = document.createElement('button');
+      btn.style.width = this.width + 'px';
+      btn.style.height = this.height + 'px';
+      btn.style.backgroundColor = this.background;
+      btn.textContent = this.value;
+      btn.style.marginRight = 20 + 'px';
+      btn.style.borderRadius = this.borderRadius + '%';
+      document.body.appendChild(btn);
+   }
+}
+
+let btnMod = new ModernButton(300, 300, 'yellow', 'working', 50.49);
+btnMod.render();
+console.log(btnMod);
