085a25b436f499bd64e0458aa9a2565b8e8df151
Screppy Coco <kozel95@bk.ru>
2022-03-13 01:47:55 +0300

    completed tasks from unit_6

--- WebDev_OOP/unit_07/app/script.js
+++ WebDev_OOP/unit_07/app/script.js
@@ -1,16 +1,10 @@
 console.log(this); // window
 // arrow function -> this -> window
 
-// document.querySelector('.b-1').onclick = function () {
-//     console.log(this);
-//     this.style.background = 'orange';
-// }
-
-document.querySelector('.b-1').addEventListener('click', f1);
-
+document.querySelector('.b-1').addEventListener ('click', f1) 
 
 function f1() {
-    console.log(this);
+    console.log(this)
     this.style.background = 'orange';
 }
 
@@ -19,29 +13,30 @@ for (let i = 0; i < p.length; i++) {
     p[i].onclick = f1;
 }
 
-class U2 {
-    constructor(model) {
+class Test { 
+    constructor(name ,model) {
+        this.name = name;
         this.model = model;
     }
     showThis() {
-        console.log(this);
+        console.log(this)
     }
 }
 
-const boat = new U2('744');
-const boat2 = new U2(333);
+const boat = new Test('smb', '744');
+const boat2 = new Test('smb', 333);
 console.log(boat);
 console.log(boat2);
 boat.showThis();
 boat2.showThis();
 
-class YellowSubmarine extends U2 {
-    constructor(model, color) {
-        super(model);
+class TestHeir extends Test {
+    constructor(name, color) {
+        super(name);
         this.color = color;
-        console.log('------------')
-        console.log(this.model);
+        console.log(this.name)
     }
 }
 
-let b = new YellowSubmarine(666, 'red');
+let b = new TestHeir('tnk', 'red');
+console.log(b)
