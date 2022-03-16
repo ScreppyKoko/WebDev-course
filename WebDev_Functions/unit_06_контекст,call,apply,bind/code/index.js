54d377195c72b3496ad855c3fc647b161e5d733a
Alex Chernyshov <kozel95@bk.ru>
2022-03-16 11:24:42 +0300

    complete code from lesson for unit_6

--- WebDev_Functions/unit_06_контекст,call,apply,bind/code/index.js
+++ WebDev_Functions/unit_06_контекст,call,apply,bind/code/index.js
@@ -1,5 +1,5 @@
-// контекст = область видимости + переменная this
-// this  - ссылка на объект, который вызывает код в данный момент
+//! контекст = область видимости + переменная this
+//! this  - ссылка на объект, который вызывает код в данный момент
 
 let count = 0;
 
@@ -12,7 +12,7 @@ function f1() {
 
 // document.querySelector('.b-1').addEventListener('click', f1);
 
-// arrow не имеют this
+//! arrow не имеют this
 // const f2 = () => {
 //     console.log(count);
 //     console.log(this);
@@ -27,15 +27,15 @@ function f1() {
 // f1.call(document.querySelector('.b-1'));
 // f1.call(document.querySelector('.b-2'));
 
-document.querySelector('.b-1').addEventListener('click', ()=>{
+document.querySelector('.b-1').addEventListener('click', () => {
     f1.call(document.querySelector('.b-2'));
 });
 
 
-function f3(count) {
-    console.log(count);
+function f3(prop) {
+    console.log(prop);
     console.log(this);
-    this.textContent = count;
+    this.textContent = prop;
 }
 
 document.querySelector('.b-3').addEventListener('click', () => {
@@ -43,55 +43,55 @@ document.querySelector('.b-3').addEventListener('click', () => {
     f3.call(document.querySelector('.b-2'), count);
 });
 
-function sum1(a,b) {
+function sum1(a, b) {
     this.innerHTML = a + b;
 }
 
-document.querySelector('.b-2').addEventListener('click', ()=>{
-    sum1.call(document.querySelector('.out-3'), 13,4);
-    sum1.apply(document.querySelector('.out-4'), [13,4]);
+document.querySelector('.b-2').addEventListener('click', () => {
+    sum1.call(document.querySelector('.out-3'), 13, 4);
+    sum1.apply(document.querySelector('.out-4'), [2, 4]);
 });
 
-// bind
-const f4 = f1.bind(document.querySelector('.out-5'));
-f4();
-f4();
-f4();
-document.querySelector('.b-5').addEventListener('click', f4);
+//! bind
+const f5 = f1.bind(document.querySelector('.out-5'));
+// f5();
+// f5();
+// f5();
+document.querySelector('.b-5').addEventListener('click', f5);
 
 const sum2 = sum1.bind(document.querySelector('.out-6'));
-sum2(4,5);
-sum2(4,15);
+// sum2(4, 5);
+sum2(4, 15);
 
-// Частичные функции, переопределенным аргументом (частичные вычисления)
+//! Частичные функции, переопределенным аргументом (частичные вычисления)
 
 function sum3(a, b, c) {
     console.log(arguments);
     this.innerHTML = a + b + c;
 }
 
-const sum4 = sum3.bind(document.querySelector('.b-6'),100,200);
+const sum4 = sum3.bind(document.querySelector('.b-6'), 100, 200);
 
-document.querySelector('.b-6').addEventListener('click', ()=>{
-    sum4(5);
+document.querySelector('.b-6').addEventListener('click', () => {
+    sum4(5, 8, 10);  //! аргументы 8 и 10 перезатерты, т.к. при вызове переданы аргументы 100(теперь a) и 200(тепперь b), они подставляютя в начало 
 });
 
-function sum7(a,b,c) {
+function sum7(a, b, c) {
     return a + b + c;
 }
 
-const sum8 = sum7.bind(undefined, 100,300);
+const sum8 = sum7.bind(undefined, 100, 300);
 
 document.querySelector('.b-7').addEventListener('click', () => {
     document.querySelector('.out-7').textContent = sum8(15);
 });
 
-// Вытягивания методов
+//! Вытягивания методов
 
 const validate = {
     password: 'himahdddd',
-    email : 'pupkin@de',
-    isValid : false,
+    email: 'pupkin@de',
+    isValid: false,
     sayHi() {
         console.log(this);
         return (this.password.length > 6) ? true : false;
@@ -99,7 +99,7 @@ const validate = {
 }
 
 console.log(validate.sayHi());
-const obj = {password: 'hello3333'};
+const obj = { password: 'hello3333' };
 const validatePassword = validate.sayHi.bind(obj);
 
 console.log(validatePassword());
