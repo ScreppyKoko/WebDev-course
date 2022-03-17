5d5b3e99fdbc3a123f7faba056c1e1a5b0362fbf
Alex Chernyshov <kozel95@bk.ru>
2022-03-17 20:05:38 +0300

    update files

--- WebDev_Functions/unit_07_function_definition_expression,function_declaration_statement/unit_07.js
+++ WebDev_Functions/unit_07_function_definition_expression,function_declaration_statement/unit_07.js
@@ -1,45 +1,43 @@
-function sliderFunction() {
+function greatQuotesSlider() {
     const data = [
         "Начальник не всегда прав, но он всегда начальник.",
         "Время, затраченное на обсуждение проблемы обратно пропорционально значимости проблемы.",
-        "Если нужно срочно сделать какое - либо дело, обратись к тому, кто занят больше всех.",
-        "Не спеши выполнять приказ — его могут отменить.",
-        "Тому, кто сам ничего не делает, все кажется по плечу.",
-        "Начальник — это человек, который приходит на службу поздно, когда ты приходишь рано, и появляется чуть свет, когда ты опаздываешь.",
-        "Только когда читаешь разъяснение ранее полученной инструкции, догадываешься, что не понял не самой инструкции, ни разъяснений к ней.",
-        "Если отложить дело надолго, то его либо выполнит кто - нибудь другой, либо оно вообще перестанет быть нужным.",
-        "Не будь незаменимым — тебя никогда не повысят.",
-        "Позади всякого, кто сделал успешную карьеру, стоит озадаченная женщина.",
+        // "Если нужно срочно сделать какое - либо дело, обратись к тому, кто занят больше всех.",
+        // "Не спеши выполнять приказ — его могут отменить.",
+        // "Тому, кто сам ничего не делает, все кажется по плечу.",
+        // "Начальник — это человек, который приходит на службу поздно, когда ты приходишь рано, и появляется чуть свет, когда ты опаздываешь.",
+        // "Только когда читаешь разъяснение ранее полученной инструкции, догадываешься, что не понял не самой инструкции, ни разъяснений к ней.",
+        // "Если отложить дело надолго, то его либо выполнит кто - нибудь другой, либо оно вообще перестанет быть нужным.",
+        // "Не будь незаменимым — тебя никогда не повысят.",
+        // "Позади всякого, кто сделал успешную карьеру, стоит озадаченная женщина.",
     ];
-
     const bgColor = ['#CC0000', '#FF6600', '#FF3366', '#9933CC', '#003333', '#708090'];
-
     let count = 0;
     let slideCollection;
 
-    function startFunction(event) {
+    const programStart = function (event) {
         event.stopPropagation();
         this.classList.add('hide');
-        start();
-    }
+        renderQuote();
+    };
 
-    function start() {
-        const outField = document.querySelector('body');
+    const renderQuote = () => {
+        const outputField = document.querySelector('.wrap');
         for (let i = 0; i < data.length; i++) {
-            let div = document.createElement('div');
-            div.classList.add('slide');
-            if (i !== 0) div.classList.add('hide');
-            if (i === 0) div.style.background = bgColor[randomInteger(0, bgColor.length - 1)];
-            let text = document.createElement('div');
-            text.textContent = data[i];
-            div.append(text);
-            outField.append(div);
+            let blockForSlide = document.createElement('div');
+            blockForSlide.classList.add('slide');
+            if (i !== 0) blockForSlide.classList.add('hide');
+            if (i === 0) blockForSlide.style.background = bgColor[randomInteger(0, bgColor.length - 1)];
+            let quoteInSlide = document.createElement('div');
+            quoteInSlide.textContent = data[i];
+            blockForSlide.append(quoteInSlide);
+            outputField.append(blockForSlide);
         }
-        outField.onclick = showSlide;
+        outputField.addEventListener('click', showSlide);
         slideCollection = document.querySelectorAll('.slide');
-    }
+    };
 
-    function showSlide(event) {
+    const showSlide = (event) => {
         hideSingleSlide(count);
         if (count + 1 < data.length) {
             count++;
@@ -48,25 +46,24 @@ function sliderFunction() {
             count = 0;
         }
         showSingleSlide(count);
-    }
+    };
 
-    function showSingleSlide(n) {
+    const showSingleSlide = (n) => {
         slideCollection[n].classList.remove('hide');
         slideCollection[n].style.background = bgColor[randomInteger(0, bgColor.length - 1)];
-    }
+    };
 
-    function hideSingleSlide(n) {
+    const hideSingleSlide = (n) => {
         slideCollection[n].classList.add('hide');
+    };
 
-    }
-
-    function randomInteger(min, max) {
+    const randomInteger = (min, max) => {
         // случайное число от min до (max+1)
-        let rand = min + Math.random() * (max + 1 - min);
-        return Math.floor(rand);
-    }
+        let randomNum = min + Math.random() * (max + 1 - min);
+        return Math.floor(randomNum);
+    };
 
-    document.querySelector('.btn-start').onclick = startFunction;
+    document.querySelector('.btn-start').addEventListener('click', programStart);
 }
 
-document.addEventListener("DOMContentLoaded", sliderFunction);
+document.addEventListener("DOMContentLoaded", greatQuotesSlider);
